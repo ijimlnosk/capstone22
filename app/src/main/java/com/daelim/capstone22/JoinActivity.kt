79656238688db7aa5d126c7663cf7b9f0070a98d
solGoodsol <skek298@naver.com>
2022-09-01 12:59:36 +0900

    commit

--- app/src/main/java/com/daelim/capstone22/JoinActivity.kt
+++ app/src/main/java/com/daelim/capstone22/JoinActivity.kt
@@ -2,10 +2,15 @@ package com.daelim.capstone22
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class JoinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+        val edtName = findViewById<EditText>(R.id.edt_name)
+        val edtEmail = findViewById<EditText>(R.id.edt_email)
+        val edtPw = findViewById<EditText>(R.id.edt_PW)
+        val edtPwOk = findViewById<EditText>(R.id.edt_PWok)
     }
 }
