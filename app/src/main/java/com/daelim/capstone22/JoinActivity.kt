e824fab5efa7e134f56c5f959ca1c6398d27f7ee
solGoodsol <skek298@naver.com>
2022-09-01 10:22:02 +0900

    commit

--- app/src/main/java/com/daelim/capstone22/JoinActivity.kt
+++ app/src/main/java/com/daelim/capstone22/JoinActivity.kt
@@ -1,12 +1,11 @@
 package com.daelim.capstone22
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class JoinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_join)
     }
 }
--- app/src/main/java/com/daelim/capstone22/LoginActivity.kt
+++ app/src/main/java/com/daelim/capstone22/LoginActivity.kt
@@ -1,11 +1,24 @@
 package com.daelim.capstone22
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val intent = Intent(this,LodingActivity::class.java)
+        startActivity(intent)
+
+        val btnJoin = findViewById<Button>(R.id.btn_userjoin)
+
+        btnJoin.setOnClickListener {
+            val intent = Intent(this,JoinActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
