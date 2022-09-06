5a006a773b06ef70798ab54ef862b61c0265a1a7
solGoodsol <skek298@naver.com>
2022-09-06 19:12:34 +0900

    commit

--- app/src/main/java/com/daelim/capstone22/LoginActivity.kt
+++ app/src/main/java/com/daelim/capstone22/LoginActivity.kt
@@ -3,22 +3,58 @@ package com.daelim.capstone22
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class LoginActivity : AppCompatActivity() {
+
+    val TAG: String = "LoginActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val intent = Intent(this,LodingActivity::class.java)
-        startActivity(intent)
+        showLodingDialog()
 
-        val btnJoin = findViewById<Button>(R.id.btn_userjoin)
+       /* val intent = Intent(this,LodingActivity::class.java)
+        startActivity(intent)*/
 
-        btnJoin.setOnClickListener {
+        btn_userjoin.setOnClickListener {
             val intent = Intent(this,JoinActivity::class.java)
             startActivity(intent)
         }
 
+        val tvInputName = tv_InputEmail.text.toString()
+        val tbInputPw = tv_InputPw.text.toString()
+
+        val sharedPreferences = getSharedPreferences("userInformation", MODE_PRIVATE)
+
+        val saveName = sharedPreferences.getString("name","").toString()
+
+        Log.d(TAG, saveName)
+
+        if(tvInputName.equals("")){
+
+        }
+        else{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(this,"로그인 완료",Toast.LENGTH_SHORT).show()
+            finish()
+        }
+    }
+    private fun showLodingDialog(){
+        val dialog = LodingDialog(this)
+        CoroutineScope(Main).launch {
+            dialog.show()
+            delay(2000)
+            dialog.dismiss()
+        }
     }
 }
--- app/src/main/java/com/daelim/capstone22/MainActivity.kt
+++ app/src/main/java/com/daelim/capstone22/MainActivity.kt
@@ -3,10 +3,18 @@ package com.daelim.capstone22
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG : String = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
