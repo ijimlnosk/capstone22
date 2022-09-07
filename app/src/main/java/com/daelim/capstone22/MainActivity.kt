e8f2e8a104906b735ec56b732bb0c117cec7347c
solGoodsol <skek298@naver.com>
2022-09-07 16:27:05 +0900

    commit

--- app/src/main/java/com/daelim/capstone22/MainActivity.kt
+++ app/src/main/java/com/daelim/capstone22/MainActivity.kt
@@ -3,13 +3,17 @@ package com.daelim.capstone22
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.daelim.capstone22.fragments.CalendarFragment
 import com.daelim.capstone22.fragments.ListFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.time.LocalDate
 import java.util.*
@@ -49,8 +53,24 @@ class MainActivity : AppCompatActivity() {
             val item = parent.getItemAtPosition(position) as ListViewItem
             Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
         }*/
+        var bnv_main = findViewById<FrameLayout>(R.id.bnv_main) as BottomNavigationView
 
-        BottomNavigationView.setupWithNavController(ListFragment.findNavController())
+        bnv_main.run {
+             setOnItemSelectedListener{
+                when(it.itemId){
+                    R.id.list_Pay_Bottom -> {
+                        val listFragment = ListFragment()
+                        supportFragmentManager.beginTransaction().replace(R.id.fl_container,listFragment).commit()
+                    }
+                    R.id.calendar_Pay_Bottom -> {
+                        val calendarFragment = CalendarFragment()
+                        supportFragmentManager.beginTransaction().replace(R.id.fl_container,calendarFragment).commit()
+                    }
+                }
+                true
+            }
+            selectedItemId = R.id.list_Pay_Bottom
+        }
 
 
     }
