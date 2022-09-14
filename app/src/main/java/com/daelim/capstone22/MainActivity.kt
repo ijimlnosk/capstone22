6afb9812b110e433717718bce2e190c330778196
solGoodsol <skek298@naver.com>
2022-09-14 10:22:13 +0900

    navifragment 설정함

--- app/src/main/java/com/daelim/capstone22/MainActivity.kt
+++ app/src/main/java/com/daelim/capstone22/MainActivity.kt
@@ -3,6 +3,7 @@ package com.daelim.capstone22
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.daelim.capstone22.databinding.ActivityMainBinding
 import com.daelim.capstone22.fragment.CalenderFragment
@@ -17,9 +18,8 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
 
-    var flag=0
-
     /*val datas = mutableListOf<ListData>()*/
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,26 +36,72 @@ class MainActivity : AppCompatActivity() {
             switchFragment()
         }*/
 
-        binding.btnList.setOnClickListener {
+        // 하단 네비게이션 바인딩
+        binding.mainNavi.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.BD -> setNaviFragment(TAG_List,ListFragment())
+                R.id.cale -> setNaviFragment(TAG_Calender,CalenderFragment())
+            }
+            true
+        }
+
+        // 기존 바인딩
+        /*binding.btnList.setOnClickListener {
             setListFragment()
         }
         binding.btnCal.setOnClickListener {
             setCalFragment()
-        }
+        }*/
     }
     // 리스트
-    private fun setListFragment(){
+   /* private fun setListFragment(){
         val transaction = supportFragmentManager.beginTransaction().add(R.id.frameLayout,ListFragment()).addToBackStack(null)
         transaction.commit()
     }
     private fun setCalFragment(){
         val transaction = supportFragmentManager.beginTransaction().add(R.id.frameLayout,CalenderFragment()).addToBackStack(null)
         transaction.commit()
-    }
+    }*/
+    // start fragment
     fun setFragment(f: ListFragment){
         val ft = supportFragmentManager.beginTransaction()
         ft.replace(R.id.frameLayout,f)
         ft.commit()
+    }
+    // 네비 프래그먼트 컨트롤
+    fun setNaviFragment(tag: String, fragment: Fragment){
+        val manager: FragmentManager = supportFragmentManager
+        val ft: FragmentTransaction = manager.beginTransaction()
+
+        // transaction에 tag로 전달된 fragment가 없을 경우 add
+        if(manager.findFragmentByTag(tag) == null){
+            ft.add(R.id.frameLayout,fragment,tag)
+        }
+
+        // manager에 add되어있는 fragment들을 변수로 할당
+        val listFt = manager.findFragmentByTag(TAG_List)
+        val calFt = manager.findFragmentByTag(TAG_Calender)
+        // 모든 fragment hide
+        if(listFt!=null){
+            ft.hide(listFt)
+        }
+        if(calFt!=null){
+            ft.hide(calFt)
+        }
+
+        // 선택한 항목에 따라 맞는 fragment show
+        if(tag == TAG_List){
+            if(listFt!=null){
+                ft.show(listFt)
+            }
+        }else if(tag == TAG_Calender){
+            if (calFt!=null){
+                ft.show(calFt)
+            }
+        }
+        //end
+        ft.commitAllowingStateLoss()
+
     }
 
     /*private fun switchFragment(){
